d7bd35127f1d3dc1d23bcd325f4a09d5c246c349
Jacob Johnston <jacobjohnston@circleci.com>
2019-05-18 19:50:58 -0500

    add change

--- .circleci/scripts/workflow-cost-estimator.sh
+++ .circleci/scripts/workflow-cost-estimator.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+cd ~/proj/workflow-cost-estimator
+
 go get -v -t -d ./..
 make build
 go test -v ./..
--- workflow-cost-estimator/workflow-cost-estimator/main.go
+++ workflow-cost-estimator/workflow-cost-estimator/main.go
@@ -19,7 +19,6 @@ import (
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
 // AWS Lambda Proxy Request functionality (default behavior)
-//
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
